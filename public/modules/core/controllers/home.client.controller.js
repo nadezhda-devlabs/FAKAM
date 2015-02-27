5898c360ca77c7777e4f9125ddab2bfe26e9d79f
Daniel Dimitrov <danielpavlov@abv.bg>
2015-02-27 21:54:26 +0200

    nothing new

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,10 +5,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+
+
+	$('img.home-head').on('click',function(){
+		console.log('gei');
+	});
+
 	}
 ]);
-
-
-$('img.home-head').on('hover',function(){
-	$('span.hidden').css('display','block');
-});
